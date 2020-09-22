b54fa605137e480cd3ec15ffe85d3f34c9e370a9
TeriMHopkins <71412924+TeriMHopkins@users.noreply.github.com>
2020-09-22 16:34:54 -0700

    Add /Basic_HTML_1 file.

---
+++ Basic_HTML-and-JavaScript_1/Basic_JavaScrip_2.js
@@ -0,0 +1,6 @@
+function My_First_Function()  {
+		var String = "Thanks for the pie!";
+		var result = String.fontcolor("red");
+		document.getElementById("Pie") .innerHTML =
+		result
+	}
---
+++ Basic_HTML-and-JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Give me pie!");
+
+function My_First_Function()  {
+		var String = "Thanks for the pie!";
+		var result = String.fontcolor("red");
+		document.getElementById("Pie") .innerHTML =
+		result
+	}
